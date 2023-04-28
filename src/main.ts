239eddc0904500d1b72e3025947cf93f88a92298
min.wan <min.wan@sinocare.com>
2023-04-28 15:13:41 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { cac } from 'cac'
+import pc from 'picocolors'
+import figlet from 'figlet'
+export {version } from '../package.json'
+const cli = cac('dm')
+cli.version(version)
+
+cli
+  .command('create', '创建一个新项目') // 增加创建指令
+  .option('-f, --force', '如果目标文件存在，则强制覆盖') // 强制覆盖
+  .action(async (cmd) => {
+   
+  })
+
+cli
+  .command('init <template-name> <project-name>', '创建一个新项目') // 增加创建指令
+  .option('-f, --force', '如果目标文件存在，则强制覆盖') // 强制覆盖
+  .action(async (templateName, projectName, cmd) => {
+    console.log('创建一个新项目')
+  })
+
+cli.help(() => {
+  console.log(
+    '\r\n' +
+      figlet.textSync('dm', {
+        font: '3D-ASCII',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true
+      })
+  )
+  console.log(`运行 ${pc.cyan('dm <command> --help')} 查看有关命令的详细用法. \r\n`)
+})
+
+cli.command('list', '查看所有模板类型').action(() => {
+  console.log('查看所有模板类型')
+})
+
+cli.parse()
---
+++ tsup.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+const config = {
+	outDir: 'dist',
+	clean: true,
+	minify: true,
+	sourcemap: false
+}
+
+export default defineConfig([{
+	entry: ['src/main.ts'],
+	treeshake: true,
+	format: ['cjs'],
+	...config
+}])
