c1a672fec22d2d39ea25ea003475a6f649626f0e
DenseFog <move11@126.com>
2023-06-28 20:14:46 +0800

    chore: log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,6 @@
 import pc from 'picocolors'
 import logSymbols from 'log-symbols'
-
+const MESSAGE_PREFIX = '[dense-cli]: '
 export const clg = console.log
 
 export const log = {
@@ -15,4 +15,19 @@ export const log = {
 	}
 }
 
+/**
+ * Logs a warning message.
+ * @param {string} msg
+ */
+export function warn(msg: string) {
+	console.warn(MESSAGE_PREFIX + msg)
+}
+
+/**
+ * Logs an error message.
+ * @param {string} msg
+ */
+export function error(msg: string) {
+	console.error(MESSAGE_PREFIX + msg)
+}
 export default log
