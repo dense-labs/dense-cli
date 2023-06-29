ce0d94ba20ee3433dc222bee7159600de709293e
DenseFog <move11@126.com>
2023-06-29 22:23:40 +0800

    chore: add git proxy cli

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import {cac} from 'cac'
-import {version, cliname} from './constants'
-import {create} from './actions'
-import {generateAscii} from './utils/asciitext'
-import type {TTemplateName} from './types'
+import { cac } from 'cac'
+import { version, cliname } from './constants'
+import { create } from './actions'
+import { generateAscii } from './utils/asciitext'
+import type { TTemplateName } from './types'
+import { parseAuthor, storeProxyConfig } from './proxy'
 
 const cli = cac(cliname)
 cli.version(version)
@@ -18,6 +19,28 @@ cli.command('init <template-name> <dir-name>', 'create a new project') // 增加
 	.option('-f, --force', 'force overwrite if target file exists') // 强制覆盖
 	.action(async (templateName: TTemplateName, dirName: string, cmd) => {
 		create(cmd)
+	})
+cli
+	.command('proxy', 'proxy user config')
+	.alias('p')
+	.option('-r, --rule <rule>', 'proxy rule (string match repository url)')
+	.option('-n, --name <name>', 'proxy name')
+	.option('-e, --email <email>', 'proxy email')
+	.option('-a, --author <author>', 'proxy name and email (xxx <xxx@xx.com>)')
+	.action(args => {
+		const config = {
+			rule: args.r,
+			name: args.n,
+			email: args.e
+		}
+		if (args.author) {
+			// const info = parseAuthor(args.author)
+			if (info) {
+				config.name = info.name
+				config.email = info.email
+			}
+		}
+		// storeProxyConfig(config)
 	})
 
 cli.help(() => {
